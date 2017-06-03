065fd721f7adc680fd0f986610296547879286a3
mike <mkovetsky@gmail.com>
2017-06-03 19:18:02 +0300

    init

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ var wasDivided = false;
 var machineState = 1;
 var needsReanalize = false;
 var currentLineNumber = 1;
-var langLexemesNames = ['program', 'begin', 'end', 'int', 'for', 'to', 'step',
+var langLexemesNames = ['program', 'begin', '@', 'end', 'int', 'for', 'to', 'step',
     'do', 'next', 'in', 'out', 'if', 'else', 'endif', ',', '=', '+', '-',
     '*', '/', '(', ')', '>', '<', '&&', '==', '!=', '<=', '>=', '||', '↑', 'idn', 'con', 'П', '!'];
 var dataTypes = ['int', 'program'];
@@ -503,40 +503,18 @@ function analyze(sourceCode) {
     printLexemes(lexemes);
     printConsts(lexemes);
     printIdns(lexemes);
-    var syntaxAnalyzer = new SyntaxAnalyzer(lexemes);
-    syntaxAnalyzer.analyze();
-    printSyntaxErrors(syntaxAnalyzer.errors);
+    // var syntaxAnalyzer = new SyntaxAnalyzer(lexemes);
+    // syntaxAnalyzer.analyze();
+    // printSyntaxErrors(syntaxAnalyzer.errors);
 
-    // 6 laba
-    const lexemeChain = lexemes.map((lexeme) => {
-        let type = '';
-        switch (lexeme.lexemeCode) {
-            case 32: type = 'idn'; break;
-            case 33: type = 'con'; break;
-            default: type = 'operation'; break;
-        }
-        return new WorkItem(lexeme.lexemeName, type);
-    });
-
-    const polizBuilder = new PolizBuilder(lexemeChain);
-    polizBuilder.build();
-    console.log(polizBuilder.poliz);
-    $('#poliz').empty();
-    polizBuilder.poliz.forEach(item => {
-        $('#poliz').append(item.token + ' ');
-    });
-    // var mpa = new MpaAnalyzer(lexemes, langLexemes);
-    // mpa.analyze();
-    // printMpaLexemes(mpa);
-
-    // let grammar = new PrecedenceRelationshipGrammar(sourceGrammar);
-    // grammar.setEquality();
-    // grammar.findAllFirstPluses();
-    // grammar.findAllLastPluses();
-    // grammar.setLess();
-    // grammar.setMore1();
-    // grammar.setMore2();
-    // console.log(grammar);
+    let grammar = new PrecedenceRelationshipGrammar(sourceGrammar);
+    grammar.setEquality();
+    grammar.findAllFirstPluses();
+    grammar.findAllLastPluses();
+    grammar.setLess();
+    grammar.setMore1();
+    grammar.setMore2();
+    console.log(grammar);
 
     Array.prototype.last = function() {
         return this[this.length-1];
@@ -545,19 +523,19 @@ function analyze(sourceCode) {
         return this.slice(0);
     };
 
-    // let risingAnalyzer = new RisingAnalyzer(grammar, lexemes);
-    // let risingAnalyzerTable = risingAnalyzer.analyze();
-    // console.log(risingAnalyzerTable);
-    //
-    // risingAnalyzerTable.forEach((row, index) => {
-    //     $('#risingTable tbody').append(
-    //         `<tr>
-    //             <td>${index + 1}</td>
-    //             <td>${row.getStack()}</td>
-    //             <td>${row.relationSign}</td>
-    //             <td>${row.getChain()}</td>
-    //         </tr>`)
-    // });
+    let risingAnalyzer = new RisingAnalyzer(grammar, lexemes);
+    let risingAnalyzerTable = risingAnalyzer.analyze();
+    console.log(risingAnalyzerTable);
+
+    risingAnalyzerTable.forEach((row, index) => {
+        $('#risingTable tbody').append(
+            `<tr>
+                <td>${index + 1}</td>
+                <td>${row.getStack()}</td>
+                <td>${row.relationSign}</td>
+                <td>${row.getChain()}</td>
+            </tr>`)
+    });
 }
 
 $('#source-code').change(function () {
